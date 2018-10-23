40b5439f7674d98c4b32b196eeae3075fd866b6f
Eddie Knight <kknight@bgea.org>
2018-10-23 10:27:35 -0400

    Added 2h melee weapons, and DRY'd out to handle similar datatypes

--- Assets/Scripts/WeaponDatabase.cs
+++ Assets/Scripts/WeaponDatabase.cs
@@ -9,25 +9,12 @@ namespace TextRPG
 {
     public class WeaponDatabase : Database
     {
-        public List<Weapon> Weapons;
+        public List<Weapon> MeleeWeapons;
 
         public void Start()
         {
-            string jsonHome = @"json/";
-            string oneHandMeleePath = jsonHome + @"1h_melee.json";
-            string twoHandMeleePath = jsonHome + @"2h_melee.json";
-
-            JToken jsonObject = Database.GetJsonFromFile(oneHandMeleePath);
-            foreach (JToken data in jsonObject["1-hand Melee"])
-            {
-                Database.LoadMeleeItems(data, Weapons);
-            }
-
-            jsonObject = Database.GetJsonFromFile(twoHandMeleePath);
-            foreach (JToken data in jsonObject["2-hand Melee"])
-            {
-                Database.LoadMeleeItems(data, Weapons);
-            }
+            Database.LoadMeleeWeaponData("1h_melee", MeleeWeapons)
+            Database.LoadMeleeWeaponData("2h_melee", MeleeWeapons)
         }
     }
 }
