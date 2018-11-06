477abfef1bbc4f1759cac7068985ec71c74ec9e9
Eddie Knight <kknight@bgea.org>
2018-11-06 17:14:16 -0500

    Typo correction

--- Assets/Scripts/ArmorDatabase.cs
+++ Assets/Scripts/ArmorDatabase.cs
@@ -6,7 +6,7 @@ namespace RpgDB
 {
     public class ArmorDatabase : Database
     {
-        public string[] ArmorCategories = { "light_armor, heavy_armor" };
+        public string[] ArmorCategories = { "light_armor", "heavy_armor" };
 
         public static List<IRpgObject> ArmorList = new List<IRpgObject>();
         public static List<Armor> Armor = new List<Armor>();
