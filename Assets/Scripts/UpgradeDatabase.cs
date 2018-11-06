f98823b0b599697a193ecc9bd65b7825f7246b40
Eddie Knight <kknight@bgea.org>
2018-11-06 17:11:45 -0500

    Added upgrades database

---
+++ Assets/Scripts/UpgradeDatabase.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using System.Linq;
+
+namespace RpgDB
+{
+    public class UpgradeDatabase : Database
+    {
+        public string[] UpgradesCategories = { "upgrades" };
+
+        public static List<IRpgObject> UpgradesList = new List<IRpgObject>();
+        public static List<Upgrade> Upgrades = new List<Upgrade>();
+
+        public void Start()
+        {
+            // Data is only loaded on first instantiation of Prefabs
+            if (Upgrades.Count < 1)
+            {
+                LoadData(UpgradesCategories, UpgradesList);
+                Upgrades = UpgradesList.Cast<Upgrade>().ToList();
+            }
+        }
+
+        // Add Object to Upgrades List
+        public override void AddObject(JToken item, List<IRpgObject> list, string category)
+        {
+            Upgrade Upgrade = new Upgrade();
+            Upgrade.ConvertObject(item, category);
+            list.Add(Upgrade);
+        }
+
+        // TODO: Build Search Functions.
+
+    }
+}
