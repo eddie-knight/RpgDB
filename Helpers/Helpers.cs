38b30acee36978529b17bc77ef009c44499c6ca0
Eddie Knight <kknight@bgea.org>
2018-12-05 16:51:46 -0500

    Moved Roll logic to helpers directory

---
+++ Helpers/Helpers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace RpgDB
+{
+    public class Helpers
+    {
+        private Random Random = new Random();
+
+        public int RollDie(int die)
+        {
+            return Random.Next(0, die);
+        }
+    }
+}
