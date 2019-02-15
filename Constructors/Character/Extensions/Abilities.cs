31cda3d03f31e2ddcafbe6cbe8ced9fe37833153
Eddie Knight <kknight@bgea.org>
2019-02-15 17:38:39 -0500

    Set default to 10

--- Constructors/Character/Extensions/Abilities.cs
+++ Constructors/Character/Extensions/Abilities.cs
@@ -2,11 +2,11 @@
 {
     public class Abilities
     {
-        public int STR;
-        public int DEX;
-        public int CON;
-        public int INT;
-        public int WIS;
-        public int CHA;
+        public int STR = 10;
+        public int DEX = 10;
+        public int CON = 10;
+        public int INT = 10;
+        public int WIS = 10;
+        public int CHA = 10;
     }
 }
